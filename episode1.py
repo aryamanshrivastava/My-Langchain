18e117cee7111008afa64e945aa962ac872e7af5
aryaman <shrivastavaaryaman0207@gmail.com>
2024-07-18 23:26:16 +0530

    ep2 updated

--- episode1.py
+++ episode1.py
@@ -1,70 +1,25 @@
-# pip install langchain openai
+# pip install langchain_openai langchain_core
 
-import os
+# Import the necessary modules
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import LLMChain, SequentialChain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-# Set OpenAI API key
-os.environ["OPENAI_API_KEY"] = "..."  # Replace with your OpenAI API key
+# Initialize the language model
+llm = ChatOpenAI(api_key="...")
 
-# Prompt Templates
-first_input_prompt = PromptTemplate(
-    input_variables=['name'],
-    template="Tell me about celebrity {name}"
-)
+# Define the prompt template
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a world class technical documentation writer."),
+    ("user", "{input}")
+])
 
-# Prompt Templates
-second_input_prompt = PromptTemplate(
-    input_variables=['person'],
-    template="When was {person} born"
-)
+# Create a function to accept user input and invoke the language model
+def get_response(user_input):
+    chain = prompt | llm | StrOutputParser()
+    return chain.invoke({"input": user_input})
 
-# Prompt Templates
-third_input_prompt = PromptTemplate(
-    input_variables=['dob'],
-    template="Mention 5 major events that happened around {dob} in the world"
-)
-
-# Memory
-person_memory = ConversationBufferMemory(input_key='name', memory_key='chat_history')
-dob_memory = ConversationBufferMemory(input_key='person', memory_key='chat_history')
-descr_memory = ConversationBufferMemory(input_key='dob', memory_key='description_history')
-
-# OpenAI LLM
-llm = ChatOpenAI(temperature=0.8, api_key=os.environ["OPENAI_API_KEY"])
-
-# Chains
-chain = LLMChain(
-    llm=llm, prompt=first_input_prompt, verbose=True, output_key='person', memory=person_memory
-)
-
-# Chains
-chain2 = LLMChain(
-    llm=llm, prompt=second_input_prompt, verbose=True, output_key='dob', memory=dob_memory
-)
-
-# Chains
-chain3 = LLMChain(
-    llm=llm, prompt=third_input_prompt, verbose=True, output_key='description', memory=descr_memory
-)
-
-# Chains
-parent_chain = SequentialChain(
-    chains=[chain, chain2, chain3], input_variables=['name'], output_variables=['person', 'dob', 'description'], verbose=True
-)
-
-# Function to run the chain
-def search_celebrity(name):
-    result = parent_chain({'name': name})
-    return result, person_memory.buffer, descr_memory.buffer
-
-# Example usage
-if __name__ == "__main__":
-    celebrity_name = input("Enter the name of the celebrity: ")
-    if celebrity_name:
-        result, person_info, major_events = search_celebrity(celebrity_name)
-        print("Result:", result)
-        print("\nPerson Info:", person_info)
-        print("\nMajor Events:", major_events)
+# Get user input and print the response
+user_input = input("Enter your text: ")
+response = get_response(user_input)
+print("Result:", response)
